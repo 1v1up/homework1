08a85f399c862248e7bbd468189d974dc84c43b7
1v1up <tlagulek@gmail.com>
2022-03-15 12:11:24 +0600

    class 1

---
+++ hello.py
@@ -0,0 +1,41 @@
+
+a = 8
+b = 7
+c = 0.4
+d = 3
+e = 5
+print ((a -e **(b / d)) % c)
+
+"""print (17 *3 > 12 * 5)
+print (12 ** 3 > 13 * 7)
+print (4 ** 5 > 512 +512)
+a = 17925 
+print (a >= 34 ** 2, a >= 26 * 3,a >= 17 * 33, a >= 4394 * 4 )
+b = 5
+c = 6
+print (( b + c * 3) ** 2 - 193432)
+"""
+"""
+a = 5 
+b = 4
+c = 6
+print (a > b, b < c)
+print (7 % 3 * 4.8)
+e = 5
+f = 6
+print ( e != f )
+"""
+
+"""
+print (21 // 10,"otvet 2 potomy chto 21 delitsya dva raza na dva " )
+"""
+'''
+a = 1995
+b = 2022
+print ( b - a + 2 , b - a - 2)
+
+a = 26
+b = 100
+print (( a /  b) * 100 % 100)
+print ((25 + 75 +  10 + 95) / 4)
+'''
